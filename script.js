b72051298a9284a48703e9b43f499d761f1b45b4
ANAKARLA VAMSI <123172204+vamshi1698@users.noreply.github.com>
2024-03-31 17:16:17 +0000

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+para = document.getElementById('para');
+trans = document.getElementById('translate');
+trans.addEventListener('click',()=>{
+        if(para.textContent == "It Started a Little Early For YOU🤣"){
+            para.textContent = "Neeku Konchem Early gaa Start Ayyindhi le 🤣";
+        }
+  else{
+    para.textContent = "It Started a Little Early For YOU🤣";
+  }
+});
